70a783927a98244a9fc48c8df5188fa55b35ddad
R4GN4R <jimchr2001@gmail.com>
2019-11-22 14:09:17 -0500

    Update Parrot_Os_Installer.sh
    Also except of python2.7, you should add pip install termcolor because when I done all the steps,when I run it,it gave me termcolo error

--- Parrot_Os_Installer.sh
+++ Parrot_Os_Installer.sh
@@ -42,11 +42,12 @@ echo "                                                                          
 
 function linux() {
 echo -e "$red [$green+$red]$off Installing Python ...";
-pip install python2.7
+pip install python
 echo -e "$red [$green+$red]$off Installing Modules ...";
 pip install -r requirements.txt
 pip install httplib2
 pip install mechanize
+pip install termcolor
 
 echo -e "$red [$green+$red]$off Checking directories..."
 if [ -d "/usr/share/ScreamingCobra" ]; then
